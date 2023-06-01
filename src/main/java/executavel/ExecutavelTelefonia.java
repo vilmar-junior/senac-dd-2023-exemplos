8c0c92e1bfc9130631cf756ff577289d9db69c41
vilmar.junior <vilmar.junior@FLE207D01.alunos.sc.senac.br>
2023-06-01 08:31:11 -0300

    Agora vai (2)

--- src/main/java/executavel/ExecutavelTelefonia.java
+++ src/main/java/executavel/ExecutavelTelefonia.java
@@ -19,7 +19,7 @@ public class ExecutavelTelefonia {
 		
 		Endereco endereco1 = new EnderecoController().consultarPorId(1);
 		
-		//Alterei
+		//Alterei mais uma vez
 		
 		ClienteController controladorDeClientes = new ClienteController();
 		
